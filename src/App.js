4014780a9b3c37e92ef0639840d4a0f38d5326ba
cpereiramt <cpereiramt@gmail.com>
2021-02-18 18:32:10 -0400

    saving initial changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Card from './components/Card';
+
+function App() {
+  return (
+    <>
+      <div class="container">
+        <Card/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div class="card-panel hoverable" >
+        <div class="row">
+        <h4 class="col s12">Vigilante 8 2 Offense Page Tribute Release notes</h4>
+        <ul class="collapsible">
+        <li class="col s12">
+        <div class="collapsible-header"> <i class="material-icons">add_box</i> Version 1.0 
+          </div>
+          <div class="collapsible-body"> 
+              <p class="flow-text">This is the original web page version maked for web design course at Trybe as finish project
+               of block 3
+              </p>
+          <h6>Technology used </h6>
+          <p>HTML5, CSS3, JAVASCRIPT</p>
+          <a class="flow-text" href="version-1.0/homePage.html">Acess Site <i class="small material-icons">open_in_new</i> </a>
+          </div> 
+        </li>
+        </ul>
+        </div>
+        </div>
+    )
+}
+
+export default Card
